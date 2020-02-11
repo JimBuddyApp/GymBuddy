8781661789a51e1682a6c2ceaff8a13d38d8d2f8
Sam Baik <baikhsam@gmail.com>
2020-02-10 17:42:56 -0800

    added node.js functionality

---
+++ app.js
@@ -0,0 +1,18 @@
+var PORT = 3000;
+
+const express = require('express')
+const app = express();
+
+
+
+
+// Return all pages in the /static directory
+// whenever they are requested at '/'
+// e.g., http://localhost:3000/index.html
+// maps to /static/index.html on this machine
+app.use(express.static(__dirname + '/static'));
+// Start the server
+var port = process.env.PORT || PORT; // 80 for web, 3000 for development
+app.listen(port, function() {
+	console.log("Node.js server running on port %s", port);
+});
