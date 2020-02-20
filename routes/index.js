014b4cff8cc336d7faccaa007f4db6f257c7fb5f
agroddick <agroddick@users.noreply.github.com>
2020-02-19 23:41:52 -0800

    Uploaded unique photos, and first attempt at sorting algorithm on index page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
-// Retrieve account data
 var buddies = require('../buddies.json');
+var available = search(buddies);
 
 exports.view = function(request, response){
-    console.log(buddies);
+    console.log(available);
     response.render('index', buddies);
 }
+
+function search(buddies) {
+    var result = [];
+    var i = null;
+    for (i = 0; buddies.users.length > i; i += 1) {
+        if (buddies.users[i].isFriend === "0") {
+            result.push(buddies.users[i]);
+        }
+    }
+
+    return result;
+}
+
+/* NEED TO FIX THIS FILE: GET IT TO RENDER AVAILABLE INSTEAD OF BUDDIES */
